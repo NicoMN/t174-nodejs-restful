8c2fdc3f82b01f81b741d97a80d5144d7f703a87
aluno pos <alunopos@local.inatel.br>
2019-11-23 14:48:04 -0300

    Added useful Middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
 
+// Middlewares
+
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const notFound = require('./middleware/not-found');
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
+// Routes
 const taskRouter = require('./routes/tasks');
 app.use('/api/tasks', taskRouter);
-app.use(require('./middleware/not-found'));
+
+// Missing Routes
+app.use(notFound);
 
 module.exports = app;
 
