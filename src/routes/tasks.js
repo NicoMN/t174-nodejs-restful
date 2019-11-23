699b4835d6f30b767e4841c0d068bead9424d3d7
aluno pos <alunopos@local.inatel.br>
2019-11-23 13:28:25 -0300

    Added HTTP GET & POST

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.post('/', (request, response) => {
     const data = {
-        message: 'HeLlO, WorLd'
+        message: 'Handling HTTP POST requests for /api/tasks'
     };
 
     response
@@ -11,5 +11,15 @@ router.post('/', (request, response) => {
         .status(200)
         .json(data);
 });
+
+router.get('/', (request, response) => {
+    const data = {};
+        data.message = `Handling ${request.protocol} ${request.method} for /api/tasks`;
+    
+
+    response
+        .status(200)
+        .json(data);
+})
 
 module.exports = router;
